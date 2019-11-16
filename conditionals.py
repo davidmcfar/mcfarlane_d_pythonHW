660e9b4db44dd419b96b6d38e32e518b4d89dcb2
davidmcfar <55293963+davidmcfar@users.noreply.github.com>
2019-11-15 22:40:34 -0500

    Delete conditionals.py

--- conditionals.py
+++
@@ -1,9 +0,0 @@
-# check a temperature and output result
-temperature = int(input("input a number between 0 and 100"))
-
-elif temperature <= 4: 
-	print("water is solid. cuz it's frozen")
-elif temperature < 100:
-	print("water is a liquid")
-else:
-	print("water is a gas. cuz its boiling!")
